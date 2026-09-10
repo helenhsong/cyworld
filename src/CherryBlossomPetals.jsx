d73e3894a297ec472976a4bde0e44708f44ce150
Helen Song <helenhsong@gmail.com>
2026-09-10 14:34:19 +0900

    Match petals to room artwork

--- src/CherryBlossomPetals.jsx
+++ src/CherryBlossomPetals.jsx
@@ -1,15 +1,9 @@
-import petal1 from './assets/journal/cherry-petal-1.png'
-import petal2 from './assets/journal/cherry-petal-2.png'
-
-const PETAL_IMAGES = [petal1, petal2]
+import petalSprite from './assets/journal/cherry-petal-room.png'
 
 const PETALS = [
-  { left: 73, top: 12, size: 16, angle: -18, turn: 48, duration: 7.4, delay: -1.2, driftX: -94, driftY: 92, wobble: 8 },
-  { left: 81, top: 17, size: 13, angle: 14, turn: -52, duration: 8.6, delay: -5.5, driftX: -126, driftY: 116, wobble: -7 },
-  { left: 77, top: 25, size: 14, angle: -32, turn: 62, duration: 7.9, delay: -3.4, driftX: -108, driftY: 102, wobble: 6 },
-  { left: 87, top: 10, size: 12, angle: 22, turn: -66, duration: 9.2, delay: -7.1, driftX: -142, driftY: 128, wobble: -9 },
-  { left: 83, top: 30, size: 15, angle: -10, turn: 44, duration: 8.2, delay: -6.2, driftX: -116, driftY: 88, wobble: 7 },
-  { left: 89, top: 22, size: 13, angle: 28, turn: -58, duration: 7.6, delay: -2.6, driftX: -132, driftY: 110, wobble: -6 },
+  { left: 73, top: 12, size: 8, angle: -18, turn: 48, delay: 0, driftX: -94, driftY: 92, wobble: 8 },
+  { left: 81, top: 17, size: 7, angle: 14, turn: -52, delay: -4.1, driftX: -126, driftY: 116, wobble: -7 },
+  { left: 87, top: 10, size: 6, angle: 22, turn: -66, delay: -8.2, driftX: -142, driftY: 128, wobble: -9 },
 ]
 
 function petalPath({ driftX, driftY, wobble, angle, turn }) {
@@ -33,10 +27,10 @@ function petalPath({ driftX, driftY, wobble, angle, turn }) {
 export function CherryBlossomPetals() {
   return (
     <div className="pixel-petal-layer" aria-hidden="true">
-      {PETALS.map((petal, index) => (
+      {PETALS.map((petal) => (
         <img
           key={`${petal.left}-${petal.top}`}
-          src={PETAL_IMAGES[index % PETAL_IMAGES.length]}
+          src={petalSprite}
           alt=""
           className="pixel-petal"
           draggable={false}
@@ -44,7 +38,7 @@ export function CherryBlossomPetals() {
             '--petal-left': `${petal.left}%`,
             '--petal-top': `${petal.top}%`,
             '--petal-size': `${petal.size}px`,
-            '--petal-duration': `${petal.duration}s`,
+            '--petal-duration': '12.4s',
             '--petal-delay': `${petal.delay}s`,
             ...petalPath(petal),
           }}
